0a01866506287d361f299a40b7fa0538b0c85a48
Chaoshead13 <eujh.rus@gmail.com>
2024-08-16 20:55:36 +0300

    Прокидываем шаблон продуктов и получаем продукты с бекенд

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 import App from './components/app.vue';
 import router from "./router";
 import './bootstrap';
+import axios from 'axios';
 
 const app = createApp({});
 
 app.component('app', App); 
 app.use(router); 
+app.config.globalProperties.axios = axios
 app.mount('#app');
